d4c99bd5574916e820931755f9e5292d9114772a
SasiniD <sasini.divanka93@gmail.com>
2023-05-01 11:49:19 +0300

    change 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'test-app2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-app2');
+    expect(app.title).toEqual('test-app3');
   });
 
   it('should render title', () => {
